a2b7246892ccb866d053eeeb1b02da914956a30a
Dave Forgac <tylerdave@tylerdave.com>
2015-05-16 00:10:07 -0400

    add template

---
+++ TEMPLATE-cli.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from __future__ import print_function
+"""Template for a CLI program using a different package."""
+
+# import argparse
+
+
+def cli():
+    """CLI program entry point."""
+    pass
+
+if __name__ == '__main__':
+    cli()
